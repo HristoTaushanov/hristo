43d7a8d563616eb6c06053aa55192bc93df3d538
Hristo Taushanov <htaushanov@yahoo.com>
2024-12-01 14:14:37 +0200

    created dynamodb

--- lib/hristo-stack.ts
+++ lib/hristo-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,14 @@ export class HristoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HristoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const inventoryTable = new Table(this, ' inventoryTable', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST
+    })
+  
   }
+
 }
